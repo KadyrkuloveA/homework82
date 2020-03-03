1ed2bd7b8fe34f1a591925ed00edb6fd53440eaa
Amantur Kadyrkulov <emptyline03@gmail.com>
2020-03-03 18:06:06 +0600

    completed

---
+++ models/Artist.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ArtistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: String,
+});
+
+const Artist = mongoose.model('Artist', ArtistSchema);
+
+module.exports = Artist;
